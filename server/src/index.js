62f4c2b22d5a79126840ff831df3550aecb46334
PCoderHub <poojapbalachandran@gmail.com>
2026-03-15 23:20:56 +0530

    Tested POST api/user/register against 12 test cases

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,7 @@
-require("dotenv").config();
 const express = require("express");
-const { dbConnect } = require("./config/db");
 const errorHandler = require("./middleware/errorHandler");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
-
-dbConnect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +10,4 @@ app.use("/api/user", userRoutes);
 
 app.use(errorHandler);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
